64328583621ce2290144a0630463c919afcf859b
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2025-01-29 20:54:20 +0700

    feat: register input language and loading

--- src/components/RegisterInput.jsx
+++ src/components/RegisterInput.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { LocalConsumer } from '../context/LocaleContext';
+import registerTextId from '../constant/page-content-text/id/register';
+import registerTextEn from '../constant/page-content-text/en/register';
 
 class RegisterInput extends React.Component {
   constructor(props) {
@@ -9,6 +12,7 @@ class RegisterInput extends React.Component {
       name: '',
       email: '',
       password: '',
+      loading: false,
     };
 
     this.onNameChange = this.onNameChange.bind(this);
@@ -41,24 +45,60 @@ class RegisterInput extends React.Component {
     });
   }
 
-  onSubmitHandler(event) {
+  async onSubmitHandler(event) {
     event.preventDefault();
+    this.setState({ loading: true });
 
-    this.props.register({
+    await this.props.register({
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
     });
+    this.setState({ loading: false });
   }
 
   render() {
     return (
-      <form onSubmit={this.onSubmitHandler} className="register-input">
-        <input type="text" placeholder="Nama" value={this.state.name} onChange={this.onNameChange} />
-        <input type="email" placeholder="Email" value={this.state.email} onChange={this.onEmailChange} />
-        <input type="password" placeholder="Password" autoComplete="current-password" value={this.state.password} onChange={this.onPasswordChange} />
-        <button type="submit">Register</button>
-      </form>
+      <LocalConsumer>
+        {
+          (({ locale }) => {
+            return (
+
+              <form onSubmit={this.onSubmitHandler} className="register-input">
+                <input
+                  type="text"
+                  placeholder={locale === 'id' ? registerTextId.form.name : registerTextEn.form.name}
+                  value={this.state.name}
+                  onChange={this.onNameChange}
+                />
+                <input
+                  type="email"
+                  placeholder={locale === 'id' ? registerTextId.form.email : registerTextEn.form.email}
+                  value={this.state.email}
+                  onChange={this.onEmailChange}
+                />
+                <input
+                  type="password"
+                  placeholder={locale === 'id' ? registerTextId.form.password : registerTextEn.form.password}
+                  autoComplete="current-password"
+                  value={this.state.password}
+                  onChange={this.onPasswordChange}
+                />
+                <button
+                  type="submit"
+                  disabled={this.state.loading}
+                  style={{ pointerEvents: this.state.loading ? 'none' : 'auto' }}
+                >
+                  {this.state.loading && 'Loading...'}
+                  {!this.state.loading && (locale === 'id'
+                    ? registerTextId.submit_btn
+                    : registerTextEn.submit_btn)}
+                </button>
+              </form>
+            );
+          })
+        }
+      </LocalConsumer>
     );
   }
 }
