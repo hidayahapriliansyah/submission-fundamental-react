e6b72319990baacfd5a31d60840f1aa70fcc52b4
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2025-01-30 11:15:07 +0700

    fix: login input using func component & hook

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,89 +1,57 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
-import { LocalConsumer } from '../context/LocaleContext';
+import LocaleContext from '../context/LocaleContext';
 import loginTextId from '../constant/page-content-text/id/login';
 import loginTextEn from '../constant/page-content-text/en/login';
+import useMutateApi from '../hooks/useMutateApi';
 
-class LoginInput extends React.Component {
-  constructor(props) {
-    super(props);
+function LoginInput({ login }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { isLoading, mutate: loginMutate } = useMutateApi(login);
 
-    this.state = {
-      email: '',
-      password: '',
-      loading: false,
-    };
+  const { locale } = useContext(LocaleContext);
 
-    this.onEmailChangeHandler = this.onEmailChangeHandler.bind(this);
-    this.onPasswordChangeHandler = this.onPasswordChangeHandler.bind(this);
-    this.onSubmitHandler = this.onSubmitHandler.bind(this);
-  }
+  const onEmailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
 
-  onEmailChangeHandler(event) {
-    this.setState(() => {
-      return {
-        email: event.target.value,
-      };
-    });
-  }
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
 
-  onPasswordChangeHandler(event) {
-    this.setState(() => {
-      return {
-        password: event.target.value,
-      };
-    });
-  }
-
-  async onSubmitHandler(event) {
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
-    this.setState({ loading: true });
 
-    await this.props.login({
-      email: this.state.email,
-      password: this.state.password,
-    });
+    await loginMutate({ email, password });
+  };
 
-    this.setState({ loading: false });
-  }
+  const text = locale === 'id' ? loginTextId : loginTextEn;
 
-  render() {
-    return (
-      <LocalConsumer>
-        {
-          (({ locale }) => {
-            return (
-              <form onSubmit={this.onSubmitHandler} className="login-input">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  value={this.state.email}
-                  onChange={this.onEmailChangeHandler}
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onPasswordChangeHandler}
-                  autoComplete="current-password"
-                />
-                <button
-                  type="submit"
-                  disabled={this.state.loading}
-                  style={{ pointerEvents: this.state.loading ? 'none' : 'auto' }}
-                >
-                  {this.state.loading && 'Loading...'}
-                  {!this.state.loading && (locale === 'id'
-                    ? loginTextId.submit_btn
-                    : loginTextEn.submit_btn)}
-                </button>
-              </form>
-            );
-          })
-        }
-      </LocalConsumer>
-    );
-  }
+  return (
+    <form onSubmit={onSubmitHandler} className="login-input">
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={onEmailChangeHandler}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={onPasswordChangeHandler}
+        autoComplete="current-password"
+      />
+      <button
+        type="submit"
+        disabled={isLoading}
+        style={{ pointerEvents: isLoading ? 'none' : 'auto' }}
+      >
+        {isLoading ? 'Loading...' : text.submit_btn}
+      </button>
+    </form>
+  );
 }
 
 LoginInput.propTypes = {
